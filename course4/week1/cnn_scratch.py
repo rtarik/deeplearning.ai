23f352ea84798cd764ac3fad094c6198b30d02c8
delabourdonnais <rahmatallahtarik@gmail.com>
2018-05-02 09:36:04 +0800

    Implement the forward pass of a convolutional neural network from scratch

---
+++ course4/week1/cnn_scratch.py
@@ -0,0 +1,100 @@
+import numpy as np
+import math
+
+def zero_pad(X, pad):
+    """
+    Pad with zeros all images of the dataset X. The padding is applied to the height and width of an image, 
+    as illustrated in Figure 1.
+    
+    Argument:
+    X -- python numpy array of shape (m, n_H, n_W, n_C) representing a batch of m images
+    pad -- integer, amount of padding around each image on vertical and horizontal dimensions
+    
+    Returns:
+    X_pad -- padded image of shape (m, n_H + 2*pad, n_W + 2*pad, n_C)
+    """
+    X_pad = np.pad(X, ((0, 0), (pad, pad), (pad, pad), (0, 0)), 'constant', constant_values=0)
+    return X_pad
+
+def conv_single_step(a_slice_prev, W, b):
+    """
+    Apply one filter defined by parameters W on a single slice (a_slice_prev) of the output activation 
+    of the previous layer.
+    
+    Arguments:
+    a_slice_prev -- slice of input data of shape (f, f, n_C_prev)
+    W -- Weight parameters contained in a window - matrix of shape (f, f, n_C_prev)
+    b -- Bias parameters contained in a window - matrix of shape (1, 1, 1)
+    
+    Returns:
+    Z -- a scalar value, result of convolving the sliding window (W, b) on a slice x of the input data
+    """
+    Z = np.sum(a_slice_prev * W + b)
+    return Z
+
+def conv_forward(A_prev, W, b, hparameters):
+    """
+    Implements the forward propagation for a convolution function
+    
+    Arguments:
+    A_prev -- output activations of the previous layer, numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    W -- Weights, numpy array of shape (f, f, n_C_prev, n_C)
+    b -- Biases, numpy array of shape (1, 1, 1, n_C)
+    hparameters -- python dictionary containing "stride" and "pad"
+        
+    Returns:
+    Z -- conv output, numpy array of shape (m, n_H, n_W, n_C)
+    cache -- cache of values needed for the conv_backward() function, namely all this function's parameters
+    """
+    stride = hparameters["stride"]
+    pad = hparameters["pad"]
+    f = W.shape[0]
+    n_C = W.shape[3]
+    A_prev_pad = zero_pad(A_prev, pad)
+    m, n_H_prev_pad, n_W_prev_pad, _ = A_prev_pad.shape
+    n_H = math.floor((n_H_prev_pad - f) / stride) + 1
+    n_W = math.floor((n_W_prev_pad - f) / stride) + 1
+    Z = np.zeros((m, n_H, n_W, n_C))
+    for i in range(m):
+        for h in range(n_H):
+            for w in range(n_W):
+                for c in range(n_C):
+                    A_slice_prev = np.squeeze(A_prev_pad[i, h*stride:h*stride+f, w*stride:w*stride+f, :])
+                    W_channel = np.squeeze(W[:, :, :, c])
+                    Z[i, h, w, c] = conv_single_step(A_slice_prev, W_channel, b[:, :, :, c])
+    cache = (A_prev, W, b, hparameters)
+    return Z, cache
+
+def pool_forward(A_prev, hparameters, mode = "max"):
+    """
+    Implements the forward pass of the pooling layer
+    
+    Arguments:
+    A_prev -- Input data, numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    hparameters -- python dictionary containing "f" and "stride"
+    mode -- the pooling mode you would like to use, defined as a string ("max" or "average")
+    
+    Returns:
+    A -- output of the pool layer, a numpy array of shape (m, n_H, n_W, n_C)
+    cache -- cache used in the backward pass of the pooling layer, contains the input and hparameters 
+    """
+    m, n_H_prev, n_W_prev, n_C = A_prev.shape
+    f = hparameters["f"]
+    stride = hparameters["stride"]
+    n_H = math.floor((n_H_prev - f) / stride) + 1
+    n_W = math.floor((n_W_prev - f) / stride) + 1
+    A = np.zeros((m, n_H, n_W, n_C))
+    for i in range(m):
+        for h in range(n_H):
+            for w in range(n_W):
+                for c in range(n_C):
+                    A_slice_prev = np.squeeze(A_prev[i, h*stride:h*stride+f, w*stride:w*stride+f, c])
+                    if mode == "max":
+                        A[i, h, w, c] = np.max(A_slice_prev)
+                    elif mode == "average":
+                        A[i, h, w, c] = np.mean(A_slice_prev)
+    cache = (A_prev, hparameters)
+    return A, cache
+    
+    
+    
